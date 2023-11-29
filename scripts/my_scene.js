de346b6f9bfedfb7c7ccbd38fd5ccb1469a45fce
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2023-11-29 15:03:01 +0900

    演習1-5の作成

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -13,6 +13,7 @@ class MyScene extends Phaser.Scene {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('background', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
+        this.load.image('jiro', 'assets/taro.png');
     }
 
     // シーン初期化処理
@@ -20,24 +21,40 @@ class MyScene extends Phaser.Scene {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(400, 300, 'background');
         this.player1 = this.add.image(400, 300, 'taro');
+        this.player2 = this.add.image(400, 400, 'jiro');
         this.player_direction1 = 1;
         this.player1.angle = 0;
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
     }
+
+    arrow_move(cursors, object){
+         if(cursors.left.isDown){
+            console.log("Left");
+            this.player1.x -= 10;
+            this.player2.x += 10;
+        }else if(cursors.right.isDown){
+            console.log("Right!!");
+            this.player1.x += 10;
+            this.player2.x -= 10;
+        }
+    }
     
   // 毎フレーム実行される繰り返し処理
     update() {
-        this.player1.angle += 50;
-        // if(this.player1.y >= D_HEIGHT - 100) this.player_direction1 = -1;
-        if(this.player1.y <= 100) this.player_direction1 = 1;
-        if(this.player_direction1 == 1) {
-            this.player1.y -= 5
-        }
+        // this.player1.angle += 50;
+        // // if(this.player1.y >= D_HEIGHT - 100) this.player_direction1 = -1;
+        // if(this.player1.y <= 100) this.player_direction1 = 1;
+        // if(this.player_direction1 == 1) {
+        //     this.player1.y -= 5
+        // }
 
-        if(this.player1x <= 100) this.player_direction1 = 1;
-        if(this.player_direction1 == 1) {
-            this.player1.x += 5;
-        }
+        // if(this.player1x <= 100) this.player_direction1 = 1;
+        // if(this.player_direction1 == 1) {
+        //     this.player1.x += 5;
+        // }
+
+        let cursors = this.input.keyboard.createCursorKeys();
+
+        this.arrow_move(cursors,this.player1)
     }
-
 }
